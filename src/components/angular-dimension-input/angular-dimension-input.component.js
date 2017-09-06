884ba39bc249c3fdd04c165db1ccca2ec2a2df27
Maxime Neger <maxime.neger@outlook.fr>
2017-09-06 11:45:28 +0200

    add ng-required

--- src/components/angular-dimension-input/angular-dimension-input.component.js
+++ src/components/angular-dimension-input/angular-dimension-input.component.js
@@ -8,13 +8,14 @@
                     '<md-input-container>' +
                         '<label>{{vm.label}}</label>' +
                         '<div layout="row" flex layout-align="center center">' +
-                            '<input flex-order="1" type="number" ng-model="vm.model.value" ng-maxlength="vm.precision" step="{{vm.step}}">' +
+                            '<input ng-required="vm.required" flex-order="1" type="number" ng-model="vm.model.value" ng-maxlength="vm.precision" step="{{vm.step}}">' +
                             '<span flex-order="2" class="md-body-2 angular-dimension-input-unit">{{vm.model.unit}}</span>' +
                        '</div>' +
                        '<div ng-messages="angularDimension.$error">' +
                             '<div ng-message="maxlength">{{\'ERROR_MAX\' | translate: {count: vm.precision} }}</div>' +
                             '<div ng-message="step">{{\'ERROR_STEP\' | translate: {count: vm.scale} }}</div>' +
                             '<div ng-message="number">{{\'ERROR_NUMBER\' | translate }}</div>' +
+                            '<div ng-message="required">{{\'ERROR_REQUIRED\' | translate: {label: vm.label} }}</div>' +
                        '</div>' +
                     '</md-input-container>' +
                   '</div>',
@@ -22,7 +23,8 @@
             label: '@',
             model: '=',
             precision: '@',
-            scale: '@'
+            scale: '@',
+            required: '<'
         }
     };
 
